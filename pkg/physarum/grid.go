29ce595553eabeebf0442c209cc9498e01772736
Michael Fogleman <fogleman@gmail.com>
2020-11-13 23:23:06 -0500

    don't do bilinear add (causes biases)

--- pkg/physarum/grid.go
+++ pkg/physarum/grid.go
@@ -24,37 +24,22 @@ func NewGrid(w, h int) *Grid {
 	return &Grid{w, h, data, temp}
 }
 
-func (g *Grid) Get(x, y float32) float32 {
+func (g *Grid) Index(x, y float32) int {
 	i := int(x+float32(g.W)) & (g.W - 1)
 	j := int(y+float32(g.H)) & (g.H - 1)
-	index := j*g.W + i
-	return g.Data[index]
+	return j*g.W + i
+}
+
+func (g *Grid) Get(x, y float32) float32 {
+	return g.Data[g.Index(x, y)]
 }
 
 func (g *Grid) GetTemp(x, y float32) float32 {
-	i := int(x+float32(g.W)) & (g.W - 1)
-	j := int(y+float32(g.H)) & (g.H - 1)
-	index := j*g.W + i
-	return g.Temp[index]
+	return g.Temp[g.Index(x, y)]
 }
 
 func (g *Grid) Add(x, y, a float32) {
-	x += float32(g.W)
-	y += float32(g.H)
-	x0 := int(x)
-	y0 := int(y)
-	x1 := x0 + 1
-	y1 := y0 + 1
-	u := x - float32(x0)
-	v := y - float32(y0)
-	x0 &= (g.W - 1)
-	y0 &= (g.H - 1)
-	x1 &= (g.W - 1)
-	y1 &= (g.H - 1)
-	g.Data[x0+y0*g.W] += a * ((1 - u) * (1 - v))
-	g.Data[x0+y1*g.W] += a * ((1 - u) * v)
-	g.Data[x1+y0*g.W] += a * (u * (1 - v))
-	g.Data[x1+y1*g.W] += a * (u * v)
+	g.Data[g.Index(x, y)] += a
 }
 
 func (g *Grid) BoxBlur(radius, iterations int, decayFactor float32) {
